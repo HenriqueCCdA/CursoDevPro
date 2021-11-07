fa21fbe51425a526463a30ffe596e5d9ec3eddb4
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-11-07 19:46:56 -0300

    Lib decorator.

--- objetos_pythonicos/programacao_funcional/decorador/tempo.py
+++ objetos_pythonicos/programacao_funcional/decorador/tempo.py
@@ -1,5 +1,7 @@
+from inspect import getfullargspec
 from time import sleep, strftime
 from functools import wraps
+from decorator import decorator
 
 def logar(f):
     def executar_com_tempo(*args, **kwargs):
@@ -18,16 +20,49 @@ def logar2(f):
     return executar_com_tempo
 
 
-@logar2
+def logar3(fmt):
+    def decorator(f):
+        @wraps(f)
+        def executar_com_tempo(*args, **kwargs):
+            print(strftime(fmt))
+            return f(*args, **kwargs)
+
+        return executar_com_tempo
+    
+    return decorator
+
+
+def logar4(fn=None, *, fmt='%H:%M:%S'):
+    if fn is not None:
+        return logar4(fmt=fmt)(fn)
+
+    def decorator(f):
+        @wraps(f)
+        def executar_com_tempo(*args, **kwargs):
+            print(strftime(fmt))
+            return f(*args, **kwargs)
+
+        return executar_com_tempo
+    
+    return decorator
+
+@decorator
+def logar5(f, fmt='%H:%M:%S', *args, **kwargs):
+    print(strftime(fmt))
+    return f(*args, **kwargs)
+
+@logar5
 def mochileiro():   
     return 42
 
-@logar2
+@logar5(fmt='%d/%m/%Y %H:%M:%S')
 def ola(nome):
     return f'Ola {nome}'
 
 
 if __name__ == '__main__':
+    print(getfullargspec(mochileiro))
+    print(getfullargspec(ola))
     print(mochileiro())
     print('nome func:', mochileiro.__name__)
     print(ola('Renzo'))
