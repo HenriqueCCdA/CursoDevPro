fd76a5144dc7c5898cdb9ff7133cf3f22a5bdb20
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-11-10 23:27:36 -0300

    Objeto Cao.

---
+++ objetos_pythonicos/aulas_antigos/oopy/caes/cao.py
@@ -0,0 +1,33 @@
+class Mamifero:
+    """Vertebrados dotados de glândulas mamárias"""
+
+
+class Cao(Mamifero):
+    qt_patas = 4
+    carnivoro = True
+    nervoso = False
+
+    def __init__(self, nome):
+        self.nome = nome
+
+    def latir(self, vezes=1):
+        # quando nervoso, late o dobro
+        vezes = vezes + (self.nervoso * vezes)
+        print(self.nome + ':' + ' Au!' * vezes)
+
+    def __str__(self):
+        return self.nome
+
+    def __repr__(self):
+        return f"Cao('{self.nome}')"
+
+    def __eq__(self, outro):
+        return (isinstance(outro, Cao) and
+                self.__dict__ == outro.__dict__)
+
+class Pequines(Cao):
+    """ O pequinês está normalmente nervoso:
+        >>> fido = Pequines('Fido')
+        >>> fido.latir()
+    """
+    pass
