4a5bc4c47d37c5b843fe102fe28539060cf1964c
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-11-26 22:36:27 -0300

    Criacao funcao lambda comparando com jquery

---
+++ python_para_pythonistas/exemplos/jquery.py
@@ -0,0 +1,21 @@
+class JQuery:
+
+    def __init__(self):
+        self.funcao = None
+
+    def ready(self, funcao):
+        self.funcao = funcao
+
+    def pagina_pronta(self):
+        print('pagina pronta')
+        if self.funcao is not None:
+            self.funcao()
+
+
+jquery = JQuery()
+
+jquery.ready(lambda : print('Olá mundo'))
+
+if __name__ == '__main__':
+    print('Main')
+    jquery.pagina_pronta()
