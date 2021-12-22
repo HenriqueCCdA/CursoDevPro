0e909e01853af36e4d8deaaf94f7fd826ce1cf65
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-12-21 22:00:48 -0300

    Padrão composite.

--- python-patterns/comportamental/strategy/pedido.py
+++ python-patterns/comportamental/strategy/pedido.py
@@ -1,11 +1,19 @@
-from collections import namedtuple
+class Item:
 
+    def __init__(self, descricao, preco, quantidade):
+        self._descricao = descricao
+        self._preco = preco
+        self._quantidade = quantidade
 
-Item=namedtuple('Item', 'descricao preco quantidade')
+    def total(self):
+        return self._preco * self._quantidade
 
 class Pedido:
     def __init__(self):
         self._itens = []
 
-    def adicionar(self, item):
-        self._itens.append(item)
+    def adicionar(self, *item):
+        self._itens.extend(item)
+
+    def total(self):
+        return sum(item.total() for item in self._itens)
