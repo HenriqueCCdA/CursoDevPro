fc90808ae00b2956e9491662f1006792226a2b8b
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2022-01-02 19:37:56 -0300

    Solução 2: Resgistrando promoções com decoratos.

--- python-patterns/comportamental/strategy_pythonico/promocao.py
+++ python-patterns/comportamental/strategy_pythonico/promocao.py
@@ -1,17 +1,29 @@
 from abc import ABC, abstractmethod
 from decimal import Decimal
 
+# todos_descontos=[globals()[nome] for nome in globals() if nome.startswith('desconto_')]
+
+todos_descontos = []
+
+def promocao(promo):
+    '''
+    Registra chamável como promoção
+    '''
+
+    todos_descontos.append(promo)
+    return promo
+
 
 def desconto_null_object(pedido):
     return pedido.subtotal()
 
-
+@promocao
 def desconto_item_repetido(pedido):
     desconto = pedido.soma_dos_itens_com_quantidade_maior_que(10)
     desconto *= Decimal('0.10')
     return pedido.subtotal() - desconto
 
-
+@promocao
 def desconto_grande_pedido(pedido):
     subtotal = pedido.subtotal()
     if subtotal < 10000:
@@ -19,8 +31,6 @@ def desconto_grande_pedido(pedido):
 
     return pedido.subtotal() * Decimal('0.95')
 
-
-todos_descontos=[desconto_item_repetido, desconto_grande_pedido]
 
 def melhor_promocao(pedido):
     return min(desconto(pedido) for desconto in todos_descontos)
